1a5725663f98a154d0e14d2c6de5cf34330cdacd
kiwiyou <kiwiyou.dev@gmail.com>
2020-05-10 01:51:12 +0900

    Solve 16916 again using KMP

--- P16916.rs
+++ P16916.rs
@@ -1,18 +1,48 @@
 use std::{io::Read, fs::File, os::unix::io::FromRawFd};
-use std::mem::{MaybeUninit, transmute};
 
 fn main() {
     let mut buffer = {
-        let uninit = [MaybeUninit::<u8>::uninit(); 2_000_001];
-        unsafe { transmute::<_, [u8; 2_000_001]>(uninit) }
+        let mut uninit = Vec::<u8>::with_capacity(2_000_001);
+        unsafe { uninit.set_len(2_000_001) };
+        uninit
     };
     let mut stdin = unsafe { File::from_raw_fd(0) };
     let len = stdin.read(&mut buffer).unwrap();
-    let input = unsafe { std::str::from_utf8_unchecked(&buffer[..len]) };
-    let mut split = input.split('\n');
+    let buffer = &buffer[..len];
+    let mut split = buffer.split(|b| *b == b'\n');
     let haystack = split.next().unwrap();
     let needle = split.next().unwrap();
-    if haystack.contains(needle) {
+    let mut retry = {
+        let mut uninit = Vec::<usize>::with_capacity(needle.len());
+        unsafe { uninit.set_len(needle.len()) };
+        uninit
+    };
+    retry[0] = 0;
+    let mut offset = 0;
+    for i in 1..needle.len() {
+        while offset > 0 && needle[i] != needle[offset] {
+            offset = retry[offset - 1];
+        }
+        if needle[i] == needle[offset] {
+            offset += 1;
+            retry[i] = offset;
+        }
+    }
+    offset = 0;
+    let mut found = false;
+    for i in 0..haystack.len() {
+        while offset > 0 && haystack[i] != needle[offset] {
+            offset = retry[offset - 1];
+        }
+        if haystack[i] == needle[offset] {
+            offset += 1;
+            if offset == needle.len() {
+                found = true;
+                break;
+            }
+        }
+    }
+    if found {
         println!("1");
     } else {
         println!("0");
