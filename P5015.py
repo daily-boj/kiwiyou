a5101c3c7004ddde2e79c15542bb612a1538feff
kiwiyou <kiwiyou.dev@gmail.com>
2020-07-09 23:21:14 +0900

    Refactor 5015

--- P5015.py
+++ P5015.py
@@ -1,15 +1,17 @@
 from sys import stdin
 pattern = stdin.readline().rstrip()
+plen = len(pattern)
 file_cnt = int(stdin.readline().rstrip())
 for _ in range(file_cnt):
     file = stdin.readline().rstrip()
-    cache = [[False] * (len(file) + 1) for _ in range(len(pattern) + 1)]
-    cache[len(pattern)][len(file)] = True
-    for pi in range(len(pattern) - 1, -1, -1):
-        for fi in range(len(file), -1, -1):
+    flen = len(file)
+    cache = [[False] * (flen + 1) for _ in range(plen + 1)]
+    cache[plen][flen] = True
+    for pi in range(plen - 1, -1, -1):
+        for fi in range(flen, -1, -1):
             if pattern[pi] == '*':
-                cache[pi][fi] = cache[pi + 1][fi] or (fi < len(file) and cache[pi][fi + 1])
-            elif fi < len(file) and pattern[pi] == file[fi]:
+                cache[pi][fi] = cache[pi + 1][fi] or (fi < flen and cache[pi][fi + 1])
+            elif fi < flen and pattern[pi] == file[fi]:
                 cache[pi][fi] = cache[pi + 1][fi + 1]
     if cache[0][0]:
         print(file)
